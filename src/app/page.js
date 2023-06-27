eaffe46fc2fd749a9787f04a11c5dfd59da7db85
DiegoContrerasA <diegocontreras1219@gmail.com>
2023-06-26 23:08:34 -0500

    add standard js

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,9 @@
-export default function Home() {
+export default function Home () {
   return (
-   <main className="page">
-     <h1 className="title">Sadhana Essentials</h1>
-     <section className="main" />
-     <h3 className="text">Próximamente ...</h3>
-   </main>
+    <main className='page'>
+      <h1 className='title'>Sadhana Essentials</h1>
+      <section className='main' />
+      <h3 className='text'>Próximamente ...</h3>
+    </main>
   )
 }
