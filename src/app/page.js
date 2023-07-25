d1094c577f3daafd3b8b6a6c9fe102953a060d7e
DiegoContrerasA <diegocontreras1219@gmail.com>
2023-07-25 17:40:20 -0500

    fix irregular space

--- src/app/page.js
+++ src/app/page.js
@@ -45,9 +45,7 @@ export default function Home () {
             </Button>
           </a>
         </div>
-
       </header>
-
       <section className='py-40 bg-secondaryText px-8'>
         <div className='max-w-screen-xl m-auto text-primaryText flex flex-col items-center'>
           <h1 className='text-4xl md:text-5xl mb-5 text-center'>MasterClass: Desde la base juntos crecemos</h1>
@@ -151,7 +149,7 @@ export default function Home () {
       </section>
       <footer className='px-8 py-10 bg-[#9570a2] text-center border-t-2 text-gray-700'>
         <p className='text-xl text-gray-100 max-w-lg m-auto'>
-          Desarrollado con mucho 🤍 por <span className='text-white font-bold'>sadhana.essentials</span>  <strong className='font-sans text-white'>2023</strong>, siguenos en <a className='font-bold text-white' href='https://www.instagram.com/sadhana_essentials/' target='_blank' rel='noreferrer'>Instagram</a>
+          Desarrollado con mucho 🤍 por <span className='text-white font-bold'>sadhana.essentials</span>  <strong className='font-sans text-white'>2023</strong>, siguenos en <a className='font-bold text-white' href='https://www.instagram.com/sadhana_essentials/' target='_blank' rel='noreferrer'>Instagram</a>
         </p>
       </footer>
     </main>
