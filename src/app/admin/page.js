2bff6f51a9b816e23137531a16246ca6fdffb4a4
DiegoContrerasA <diegocontreras1219@gmail.com>
2023-08-01 22:59:05 -0500

    test remove middleware

--- src/app/admin/page.js
+++ src/app/admin/page.js
@@ -1,13 +1,8 @@
-import prisma from '@/libs/prismadb'
 const Admin = async () => {
-  const users = await await prisma.user.findMany()
-
   return (
     <>
       <h1>Admin</h1>
-      {users.map((user) => (
-        <code key={user.id}>{JSON.stringify(user, 2, null)}</code>
-      ))}
+
     </>
 
   )
