eedacc180de9566011bc33b1d4394acf10e403a6
DiegoCoontreras <diego.contreras@trend-it.com.mx>
2023-08-25 16:18:54 -0500

    add video player with loading and error

--- src/app/video/page.js
+++ src/app/video/page.js
@@ -11,7 +11,7 @@ const Video = () => {
 
   return (
     <div className='flex justify-center items-center min-h-full bg-accent bg-gradient-to-r from-[#28162b] to-[#4f2e55] py-10 px-5 flex-col'>
-      <div className='w-full max-w-5xl rounded-xl overflow-hidden shadow-sm'>
+      <div className='w-full max-w-5xl shadow-sm'>
         <VideoPlayer
           ref={videoRef}
           loading={loading}
--- src/components/VideoPlayer/index.js
+++ src/components/VideoPlayer/index.js
@@ -12,14 +12,15 @@ const VideoPlayer = forwardRef(({ loading, error, onRetry }, ref) => {
   if (error) return <ErrorPlayer onRetry={handleRentry} retry={retry < 3} />
 
   return (
-    <div className='aspect-video w-full relative rounded-xl overflow-hidden'>
+    <div className='aspect-video w-full relative rounded-sm overflow-hidden'>
       {loading && <div className='aspect-video absolute top-0 left-0 z-10 animate-pulse bg-[#d900ff45] w-full h-full' />}
       <video
         ref={ref}
         onContextMenu={(e) => e.preventDefault()}
         src=''
-        className={`aspect-ratio w-full ${loading ? 'opacity-0' : ''}`}
+        className={`aspect-video w-full  ${loading ? 'opacity-0' : ''}`}
         controls
+
       />
     </div>
   )
