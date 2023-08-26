a18e763e46d3f960461f09fdacd35aa31bfde5c2
DiegoCoontreras <diego.contreras@trend-it.com.mx>
2023-08-26 07:22:42 -0500

    add bg video container

--- src/components/VideoPlayer/index.js
+++ src/components/VideoPlayer/index.js
@@ -12,7 +12,7 @@ const VideoPlayer = forwardRef(({ loading, error, onRetry }, ref) => {
   if (error) return <ErrorPlayer onRetry={handleRentry} retry={retry < 3} />
 
   return (
-    <div className='aspect-video w-full relative rounded-sm overflow-hidden'>
+    <div className='aspect-video w-full relative rounded-sm overflow-hidden bg-[#d900ff45]'>
       {loading && <div className='aspect-video absolute top-0 left-0 z-10 animate-pulse bg-[#d900ff45] w-full h-full' />}
       <video
         ref={ref}
@@ -20,7 +20,6 @@ const VideoPlayer = forwardRef(({ loading, error, onRetry }, ref) => {
         src='https://res.cloudinary.com/dudykjuq5/video/upload/v1692930320/cc4p3ha39szr1h1f71kl.mp4'
         className={`aspect-video w-full  ${loading ? 'opacity-0' : ''}`}
         controls
-
       />
     </div>
   )
