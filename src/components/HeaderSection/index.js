71b632b232d589154a498292f72a710ac8bfed1f
diego-contreras_globant <diego.contreras@globant.com>
2023-12-18 11:08:12 -0500

    remove landing page

--- src/components/HeaderSection/index.js
+++ src/components/HeaderSection/index.js
@@ -1,52 +1,9 @@
-// import Button from '../Button'
-// import CountDown from '../CountDown'
-
 const HeaderSection = () => {
   return (
     <header className=' w-full bg-accent bg-gradient-to-b sm:bg-gradient-to-r from-[#E7E0E8] to-[#916a9f] px-8 min-h-screen overflow-hidden relative flex justify-center items-center flex-col text-[#6d4575ba]'>
-      {/* <nav className='flex justify-end p-5 z-20 max-w-screen-2xl m-auto relative min-h-[116px] xs:min-h-[80px]'> */}
       <div className='w-[300px] h-[300px] bg-secondaryText rounded-full absolute top-[-100px] left-[-100px] hero' />
       <div className='w-[300px] h-[300px] bg-secondaryText rounded-full absolute bottom-[-100px] right-[-100px] hero' />
-      {/* <CountDown /> */}
-      {/* </nav> */}
-      {/* <div className='flex flex-col md:flex-row justify-between relative z-0 max-w-screen-xl m-auto py-10 gap-10 md:py-20'>
-        <div className='w-[100%] md:w-[40%] flex-1 flex justify-center items-center'>
-          <div className='min-w-[280px] relative z-10'>
-            <h3 className='text-2xl sm:text-4xl leading-3 font-light italic text-primary'>Día de</h3>
-            <h1 className='text-[90px] xs:text-[120px] sm:text-[150px] font-bold text-primaryText leading-none'>YOGA</h1>
-            <p className='text-xl text-center sm:text-2xl font-light leading-none text-[#6d4575ba] mb-10 md:mb-20'>
-              Diciembre <strong className='font-sans italic'> 16.2023 </strong>
-            </p>
-            <Button type='link' href='#form' className='hidden md:flex  w-full min-h-[60px] '>
-              ¡Inscríbete ya!
-            </Button>
-          </div>
-        </div>
-        <div className='w-[100] md:w-[40%] flex-1 flex justify-center relative'>
-          <picture className='relative z-10'>
-            <source media='(max-width: 450px)' srcSet='images/header-xs.webp' width={280} height={370} />
-            <source media='(max-width: 640px)' srcSet='images/header-sm.webp' width={320} height={423} />
-            <img
-              src='images/header-md.webp'
-              alt='mujer con camisa negra, pantalon rojo haciendo una pose de yoga'
-              width={450} height={495}
-            />
-          </picture>
-          <div className='w-[500px] h-[500px]
-            xs:w-[600px] xs:h-[600px]
-            sm:w-[700px] sm:h-[700px]
-            md:w-[600px] md:h-[600px]
-             lg:w-[700px] lg:h-[700px]
-              bg-secondaryText absolute rounded-full hero bottom-2'
-          />
-        </div>
-        <div>
-          <Button href='#form' type='link' className='w-[220px] md:hidden mb-10 sm:mt-10 mx-auto'>
-            ¡Inscríbete ya!
-          </Button>
-        </div>
-      </div> */}
-      <h1 className='z-10 relative max-w-[90%] text-center font-extrabold text-7xl md:text-9xl'>Sadhana Essential </h1>
+      <h1 className='z-10 relative max-w-[90%] text-center font-extrabold text-7xl md:text-9xl'>Sadhana Essentials</h1>
       <h3 className='text-4xl md:text-6xl my-3'> próximamente</h3>
       <h4 className='text-2xl font-light text-center italic'>by Andrea Cardona</h4>
     </header>
